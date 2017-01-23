14a80273e5cc8aab001970afa17fffa72dae2bcc
Lindsey Ullman <lindsey.ullman@gmail.com>
2017-01-22 22:18:54 -0700

    Create views and controllers

---
+++ app/controllers/crates_controller.rb
@@ -0,0 +1,42 @@
+class CratesController < ApplicationController
+
+  def index
+    @crates = Crate.all
+  end
+
+  def show
+    @crate = Crate.find(params[:id])
+  end
+
+  def new
+    @crate = Crate.new
+  end
+
+  def edit
+  end
+
+  def create
+    p "*" * 25
+    p "Params..."
+    p params
+    p "*" * 25
+    crate = Crate.new(crate_params)
+    if crate.save
+      redirect_to crate_path crate
+    else
+      redirect_to new_crate_path
+    end
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+    def crate_params
+      params.require(:crate).permit(:crate_name, :products_attributes => [ :product_name ])
+    end
+
+end
--- app/models/crate.rb
+++ app/models/crate.rb
@@ -2,5 +2,5 @@ class Crate < ActiveRecord::Base
 
   has_many :products
 
-  accepts_nested_attributes_for :products
+  accepts_nested_attributes_for :products, allow_destroy: true
 end
